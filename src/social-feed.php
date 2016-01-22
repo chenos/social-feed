fd3c1156f37b150322ac57990c46938334adbe15
chen <chenlinxh@gmail.com>
2016-01-22 11:12:50 +0800

    Replace HTTP Request

--- src/social-feed.php
+++ src/social-feed.php
@@ -9,9 +9,9 @@ class SocialFeed {
 	public static function get_streams() {
 		$streams = array();
 
-		$facebook = json_decode(static::get_facebook_feed());
-		$twitter = json_decode(static::get_twitter_feed());
-		$instagram = json_decode(static::get_instagram_feed());
+		$facebook = static::get_facebook_feed();
+		$twitter = static::get_twitter_feed();
+		$instagram = static::get_instagram_feed();
 
 		foreach ( $facebook->data as $key => $item ) {
 			$stream['username'] = $item->from->name;
@@ -60,66 +60,71 @@ class SocialFeed {
 		return $new_streams;
 	}
 
-	private static function getSslPage( $url, $curl_option = null, $option_value = null ) {
-		$ch = curl_init();
-		if ( isset( $curl_option ) && isset( $option_value ) ) {
-			curl_setopt( $ch, $curl_option, $option_value );
-		}
-		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-		curl_setopt( $ch, CURLOPT_HEADER, false );
-		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-		curl_setopt( $ch, CURLOPT_URL, $url );
-		curl_setopt( $ch, CURLOPT_REFERER, $url );
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-		$result = curl_exec( $ch );
-		curl_close( $ch );
-
-		return $result;
-	}
-
 	private static function get_facebook_feed() {
-		//extract(sw_get_config('social.facebook'));
-		$id           = sw_get_option( 'social.facebook.id' );
-		$count        = sw_get_option( 'social.facebook.count' );
-		$access_token = sw_get_option( 'social.facebook.access_token' );
+		$id                 = sw_get_option('social.facebook.id');
+		$count              = sw_get_option('social.facebook.count');
+		$access_token       = sw_get_option('social.facebook.access_token');
+		$access_token       = is_array($access_token) ? $access_token[array_rand($access_token, 1)] : $access_token;
+		$url                = "https://graph.facebook.com/{$id}/feed?limit={$count}&access_token={$access_token}";
 
-		$access_token = is_array( $access_token ) ? $access_token[ array_rand( $access_token, 1 ) ] : $access_token;
+		for ($i=0; $i < 5; $i++) {
+				$response = wp_remote_get( $url );
+				if ( ! is_wp_error( $response ) ) {
+						return json_decode( $response['body'] );
+				}
+		}
 
-		return static::getSslPage( "https://graph.facebook.com/{$id}/feed?limit={$count}&access_token={$access_token}" );
+		return false;
 	}
 
 	private static function get_twitter_feed() {
-		$username            = sw_get_option( 'social.twitter.username' );
-		$count               = sw_get_option( 'social.twitter.count' );
-		$consumer_key        = sw_get_option( 'social.twitter.consumer_key' );
-		$consumer_secret     = sw_get_option( 'social.twitter.consumer_secret' );
-		$access_token        = sw_get_option( 'social.twitter.access_token' );
-		$access_token_secret = sw_get_option( 'social.twitter.access_token_secret' );
+		$username               = sw_get_option('social.twitter.username');
+		$count                  = sw_get_option('social.twitter.count');
+		$consumer_key           = sw_get_option('social.twitter.consumer_key');
+		$consumer_secret        = sw_get_option('social.twitter.consumer_secret');
+		$access_token           = sw_get_option('social.twitter.access_token');
+		$access_token_secret    = sw_get_option('social.twitter.access_token_secret');
 
-		$access_token = is_array( $access_token ) ? $access_token[ array_rand( $access_token, 1 ) ] : $access_token;
-		$timestamp    = time() - 1;
+		$access_token           = is_array($access_token) ? $access_token[array_rand($access_token, 1)] : $access_token;
+		$timestamp              = time() - 1;
 
-		$url          = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-		$base         = 'GET&' . rawurlencode( $url ) . '&' . rawurlencode( "count={$count}&oauth_consumer_key={$consumer_key}&oauth_nonce={$timestamp}&oauth_signature_method=HMAC-SHA1&oauth_timestamp={$timestamp}&oauth_token={$access_token}&oauth_version=1.0&screen_name={$username}" );
-		$key          = rawurlencode( $consumer_secret ) . '&' . rawurlencode( $access_token_secret );
-		$signature    = rawurlencode( base64_encode( hash_hmac( 'sha1', $base, $key, true ) ) );
-		$oauth_header = "oauth_consumer_key=\"{$consumer_key}\", oauth_nonce=\"{$timestamp}\", oauth_signature=\"{$signature}\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"{$timestamp}\", oauth_token=\"{$access_token}\", oauth_version=\"1.0\", ";
+		$url                    = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+		$base                   = 'GET&' . rawurlencode($url) . '&' . rawurlencode("count={$count}&oauth_consumer_key={$consumer_key}&oauth_nonce={$timestamp}&oauth_signature_method=HMAC-SHA1&oauth_timestamp={$timestamp}&oauth_token={$access_token}&oauth_version=1.0&screen_name={$username}");
+		$key                    = rawurlencode($consumer_secret) . '&' . rawurlencode($access_token_secret);
+		$signature              = rawurlencode(base64_encode(hash_hmac('sha1', $base, $key, true)));
+		$oauth_header           = "oauth_consumer_key=\"{$consumer_key}\", oauth_nonce=\"{$timestamp}\", oauth_signature=\"{$signature}\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"{$timestamp}\", oauth_token=\"{$access_token}\", oauth_version=\"1.0\", ";
 
-		return static::getSslPage( $url . "?screen_name={$username}&count={$count}", CURLOPT_HTTPHEADER, array(
-			"Authorization: Oauth {$oauth_header}",
-			'Expect:'
-		) );
-	}
+		$url .= "?screen_name={$username}&count={$count}";
+		$args['headers'] = array("Authorization" => "Oauth {$oauth_header}");
+
+
+		for ($i=0; $i < 5; $i++) {
+				$response = wp_remote_post( $url, $args );
+				if ( ! is_wp_error( $response ) ) {
+						return json_decode( $response['body'] );
+				}
+		}
+
+		return false;
+  }
 
 	private static function get_instagram_feed() {
-		$id           = sw_get_option( 'social.instagram.id' );
-		$count        = sw_get_option( 'social.instagram.count' );
-		$access_token = sw_get_option( 'social.instagram.access_token' );
+		$id                 = sw_get_option('social.instagram.id');
+		$count              = sw_get_option('social.instagram.count');
+		$access_token       = sw_get_option('social.instagram.access_token');
+		$access_token       = is_array($access_token) ? $access_token[array_rand($access_token, 1)] : $access_token;
+		$url                = 'https://api.instagram.com/v1/users/'.$id.'/media/recent/?count='.$count.'&access_token='.$access_token;
 
-		$access_token = is_array( $access_token ) ? $access_token[ array_rand( $access_token, 1 ) ] : $access_token;
+		for ($i=0; $i < 5; $i++) {
+				$response = wp_remote_get( $url );
+				if ( ! is_wp_error( $response ) ) {
+						return json_decode( $response['body'] );
+				}
+		}
 
-		return static::getSslPage( 'https://api.instagram.com/v1/users/' . $id . '/media/recent/?count=' . $count . '&access_token=' . $access_token );
+		return false;
 	}
+
 }
 
 $action = sw_get_config( 'social.action' );
